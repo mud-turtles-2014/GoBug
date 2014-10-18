2c9178e594d379cb7cd5616d6ae0a44da99f97bd
Sarah Taurchini <sarah.taurchini@gmail.com>
2014-10-18 16:52:09 -0400

    form shows and hides pieces

--- GoBug/app/assets/javascripts/trip.js
+++ GoBug/app/assets/javascripts/trip.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
+  $('#expense_form').hide();
 
-  $('body').on('click','.location',function(e){
-    $(this).geocomplete();
+  $('#trip_form').on('click', 'input[type=submit]', function(event){
+    $('#trip_form').hide();
+    $('#expense_form').show();
   });
+  // $('body').on('click','.location',function(e){
+  //   $(this).geocomplete();
+  // });
 
-})
+});
