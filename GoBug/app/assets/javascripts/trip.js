163aebceef956c04e266bfc6f73a4c1cccbcbdf0
Sarah Taurchini <sarah.taurchini@gmail.com>
2014-10-18 16:46:05 -0400

    form works with trip controller

--- GoBug/app/assets/javascripts/trip.js
+++ GoBug/app/assets/javascripts/trip.js
@@ -1,22 +1,4 @@
 $(document).ready(function(){
-  console.log("ready")
-
-  $('form').on('click','.remove_fields', function(event){
-    event.preventDefault();
-    $(this).prev('input[type=hidden]').val('1');
-    $(this).closest('fieldset').hide();
-  });
-
-  $('form').on('click','.add_fields', function(event){
-    event.preventDefault();
-    time = new Date().getTime();
-    regexp = new RegExp($(this).data('id'), 'g');
-    $(this).before($(this).data('fields').replace(regexp, time));
-  });
-  // $('#add-location').click(function(e){
-  //   e.preventDefault();
-  //   addLegForm();
-  // })
 
   $('body').on('click','.location',function(e){
     $(this).geocomplete();
