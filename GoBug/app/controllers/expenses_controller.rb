0ec4190e42f86e653417f0cb72bda7c856a143d1
Sarah Taurchini <sarah.taurchini@gmail.com>
2014-10-18 17:30:01 -0400

    pass to michelle

--- GoBug/app/controllers/expenses_controller.rb
+++ GoBug/app/controllers/expenses_controller.rb
@@ -1,7 +1,6 @@
 require 'pry'
 class ExpensesController < ApplicationController
   before_action :set_expense, only: [:show, :edit, :update, :destroy]
-  before_action :set_trip, only: [:new, :create]
   before_action :require_login
 
   # GET /expenses
@@ -19,7 +18,6 @@ class ExpensesController < ApplicationController
 
   # GET /expenses/new
   def new
-    @expense = Expense.new
   end
 
   # GET /expenses/1/edit
@@ -29,8 +27,8 @@ class ExpensesController < ApplicationController
   # POST /expenses
   # POST /expenses.json
   def create
-    @expense = @trip.expenses.new(expense_params)
-
+    @expense = Expense.new(expense_params)
+    binding.pry
     respond_to do |format|
       if @expense.save
         format.html { redirect_to [@expense], notice: 'Expense was successfully created.' }
@@ -70,10 +68,6 @@ class ExpensesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_expense
       @expense = Expense.find(params[:id])
-    end
-
-    def set_trip
-      @trip = Trip.find(params[:trip_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- GoBug/app/controllers/trips_controller.rb
+++ GoBug/app/controllers/trips_controller.rb
@@ -31,6 +31,7 @@ class TripsController < ApplicationController
   def create
     @current_user = current_user
     @trip = @current_user.trips.new(trip_params)
+    binding.pry
 
     respond_to do |format|
       if @trip.save
