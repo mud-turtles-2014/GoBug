8095d3ccabeef6465a0425ee4652730ceda0eecf
Michelle Chu <michu@Michelles-MacBook-Pro-2.local>
2014-10-20 12:52:59 -0400

    Add calculate total and remaining budget methods to expensables

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -1,4 +1,14 @@
 class Wishlist < ActiveRecord::Base
   has_many :expenses, as: :expensable
   belongs_to :user
+
+  def calculate_total
+    total = 0
+    self.expenses.each { |expense| total += expense.usd_cost }
+    return total
+  end
+
+  def remaining_budget
+    self.budget - self.calculate_total
+  end
 end
