902edfc25c54a1ffcadbec7a8298cf52b4c476be
Nicolle Quintero <nicolle.quintero@gmail.com>
2014-10-21 19:56:40 -0400

    add ajax call for adding to wishlist

--- app/assets/javascripts/wishlist.js
+++ app/assets/javascripts/wishlist.js
@@ -1,21 +1,29 @@
 $(document).ready(function(){
-  $('td#add').hide();
-  $('tr').hover(
-    function(){
-      $(this).children('td#add').show();
-    },
-    function(){
-      $(this).children('td#add').hide();
-    }
-  );
+
   $('select[name="wishlist_id"').change(function(e){
     e.preventDefault();
     var wishlist_id = $(this).val()
     var expense_id = $(this).parent().children('.expense_id').val()
+    
     $.ajax ({
       url: '/add_to_wishlist?expense_id=' + expense_id + '&' + 'wishlist_id=' + wishlist_id,
       type: 'POST'
     });
+  });
+
+
+  $('.add-to-wishlist').click(function(e){
+    e.preventDefault();
+    var wishlist_button = $(this).closest('.wishlist-button')
+    wishlist_button.html('<i>Adding to wishlist...  </i>')
+    
+    $.ajax ({
+      url:$(e.target).parent().parent().attr('action'),
+      type: 'POST'
+    }).done(function(response){
+      wishlist_button.html(response)
+    });
+
   });
 
 });
