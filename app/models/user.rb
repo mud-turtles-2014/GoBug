a2979ff03feae85fdad91a60a6a543cd64d3ba79
Nicolle Quintero <nicolle.quintero@gmail.com>
2014-10-21 15:55:07 -0400

    Add ability to return the ID of a wishlist item that is equal to the expense being displayed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,22 @@ class User < ActiveRecord::Base
   has_many :trips
   has_many :wishlists
   has_many :expenses, through: :trips
+
+  def has_expense_in_wishlist(target)
+  	self.wishlists.each do |wishlist|
+  	  wishlist.expenses.each do |expense|
+  		return true if Expense.have_same_info(target, expense)
+  	  end
+  	end
+  	false
+  end
+
+  def wishlist_id_of(target)
+    self.wishlists.each do |wishlist|
+      wishlist.expenses.each do |expense|
+        return expense.id if Expense.have_same_info(target, expense)
+      end
+    end
+  end
+
 end
