e577bc57f0337adf624210e446fd3add7a757689
Sarah Taurchini <sarah.taurchini@gmail.com>
2014-10-21 22:08:14 -0400

    flash alert stays longer

--- app/assets/javascripts/alerts.js
+++ app/assets/javascripts/alerts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-  $('li.alert.alert-success').fadeIn(1000);
-  $('li.alert.alert-success').fadeOut(1000);
-  $('li.alert.alert-danger').fadeIn(1000);
-  $('li.alert.alert-danger').fadeOut(1000);
+  $('li.alert.alert-success').fadeIn(2500);
+  $('li.alert.alert-success').fadeOut(2500);
+  $('li.alert.alert-danger').fadeIn(2500);
+  $('li.alert.alert-danger').fadeOut(2500);
 });
