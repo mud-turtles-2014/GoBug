b9455cc826a3778a08044ac0c4fb6fddc420e22c
Sarah Taurchini <sarah.taurchini@gmail.com>
2014-10-22 13:05:43 -0400

    remove success flashes

--- app/assets/javascripts/alerts.js
+++ app/assets/javascripts/alerts.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
-  $('li.alert.alert-success').fadeIn(2500);
-  $('li.alert.alert-success').fadeOut(2500);
   $('li.alert.alert-danger').fadeIn(2500);
   $('li.alert.alert-danger').fadeOut(2500);
 });
