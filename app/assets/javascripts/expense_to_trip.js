17d896f25d1d970150576b04b1ba7e2430ec96a1
Sarah Taurchini <sarah.taurchini@gmail.com>
2014-10-24 00:14:23 -0400

    functioning with doubling

--- app/assets/javascripts/expense_to_trip.js
+++ app/assets/javascripts/expense_to_trip.js
@@ -6,29 +6,34 @@ $(document).on('click', '.converter', function(event){
        expense.title = $(this).find('.title').html();
        expense.location = $(this).find('.loc').html();
        expense.category_id = $(this).attr('data-category');
-       // expense.currency_id = $(document.body).find('.currency-code').html();
+       expense.currency_id = $(document.body).find('.currency-code').html();
+       expense.usd_cost = $(document.body).find('p.desc.usd-cost-field').html();
+       expense.foreign_cost = $(document.body).find('.foreign-cost-field .best_in_place[data-bip-attribute="cost"]').html();
        expenses.push(expense);
        console.log(expense);
      }
-    var div = document.createElement('div');
-    div.setAttribute('id', 'form_container');
+   });
+    // var div = document.createElement('div');
+    // div.setAttribute('id', 'form_container');
 
-    var form = document.createElement('form');
-    form.setAttribute('action', '/trips/from_expenses');
-    form.setAttribute('method', 'POST');
-    form.setAttribute('id', 'exp_form');
-    div.appendChild(form);
-    $(document.body).find("#expenses_grid").append(div).hide();
+    // var form = document.createElement('form');
+    // form.setAttribute('action', '/trips/from_expenses');
+    // form.setAttribute('method', 'POST');
+    // form.setAttribute('id', 'exp_form');
+    // div.appendChild(form);
+    // $(document.body).find("#expenses_grid").append(div).hide();
 
-    for (i=0; i< expenses.length; i++) {
-      var exp = expenses[i];
-      $('<input>').attr('name', 'title_' + i).attr('value', exp.title ).appendTo('#exp_form');
-      $('<input>').attr('name', 'cat_' + i).attr('value', exp.category_id ).appendTo('#exp_form');
-      $('<input>').attr('name', 'loc_' + i).attr('value', exp.location ).appendTo('#exp_form');
-      // $('<input>').attr('name', 'cur_' + i).attr('value', exp.currency_id ).appendTo('#exp_form');
-    }
-    $('<input>').attr('type', 'hidden').attr('name', 'number_of_items').attr('value', expenses.length).appendTo('#exp_form');
-    $('<input>').attr('type', 'submit').attr('value', 'Create trip').appendTo('#exp_form');
-  });
-    $("#exp_form").submit();
+    // for (i=0; i< expenses.length; i++) {
+    //   var exp = expenses[i];
+    //   $('<input>').attr('name', 'title_' + i).attr('value', exp.title ).appendTo('#exp_form');
+    //   $('<input>').attr('name', 'cat_' + i).attr('value', exp.category_id ).appendTo('#exp_form');
+    //   $('<input>').attr('name', 'loc_' + i).attr('value', exp.location ).appendTo('#exp_form');
+    //   $('<input>').attr('name', 'cur_' + i).attr('value', exp.currency_id ).appendTo('#exp_form');
+    //   $('<input>').attr('name', 'usd_cost_' + i).attr('value', exp.usd_cost ).appendTo('#exp_form');
+    //   $('<input>').attr('name', 'foreign_cost_' + i).attr('value', exp.foreign_cost ).appendTo('#exp_form');
+    // }
+    //   $('<input>').attr('type', 'hidden').attr('name', 'number_of_items').attr('value', expenses.length).appendTo('#exp_form');
+    //   $('<input>').attr('type', 'submit').attr('value', 'Create trip').appendTo('#exp_form');
+    // });
+    // $("#exp_form").submit();
   });
