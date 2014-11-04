9c4d2167c4282e5626d1ef29665aa60b08bfafbe
Nicolle Quintero <nicolle.quintero@gmail.com>
2014-11-04 10:38:08 -0500

    comment duplicate code

--- app/assets/javascripts/user-trips-show.js
+++ app/assets/javascripts/user-trips-show.js
@@ -16,20 +16,20 @@ $(document).ready(function(){
 		$('#balance-field').text('$' + new_balance)
 	})
 
-$('#expenses_grid').on('submit','#new_expense',function(e){
-  e.preventDefault();
-  var old_balance = parseFloat($('#balance-field').text().replace(/[A-Za-z$-,]/g, ""));
-  $.ajax ({
-    url: $(e.target).attr('action'),
-    type: 'POST',
-    data: $(e.target).serialize()
-  }).done(function(data){
-    $('.event-list').append(data);
-    var usd = parseFloat($('.event-list li:last-child .usd-cost-field').text().replace(/[A-Za-z$-,]/g, ""));
-    var new_balance = (old_balance - usd).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-    $('#balance-field').text('$' + new_balance)
-    clearInputs();
-  });
-});
+// $('#expenses_grid').on('submit','#new_expense',function(e){
+//   e.preventDefault();
+//   var old_balance = parseFloat($('#balance-field').text().replace(/[A-Za-z$-,]/g, ""));
+//   $.ajax ({
+//     url: $(e.target).attr('action'),
+//     type: 'POST',
+//     data: $(e.target).serialize()
+//   }).done(function(data){
+//     $('.event-list').append(data);
+//     var usd = parseFloat($('.event-list li:last-child .usd-cost-field').text().replace(/[A-Za-z$-,]/g, ""));
+//     var new_balance = (old_balance - usd).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+//     $('#balance-field').text('$' + new_balance)
+//     clearInputs();
+//   });
+// });
 
 });
