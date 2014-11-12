916b6da99a8ca0c0f458a387282921bcfa0aa228
Nicolle Quintero <nicolle.quintero@gmail.com>
2014-11-12 18:29:18 -0500

    Fix indenting

--- app/assets/javascripts/exchange.js
+++ app/assets/javascripts/exchange.js
@@ -28,27 +28,27 @@ $(document).ready(function(){
 
 
   $.getJSON(
-        'http://openexchangerates.org/api/latest.json?app_id=10eff371738744b3b3f187281d3f9a06',
-        function(data) {
+    'http://openexchangerates.org/api/latest.json?app_id=10eff371738744b3b3f187281d3f9a06',
+    function(data) {
             // Check money.js has finished loading:
-            var currency_data = data
-            if ( typeof fx !== "undefined" && fx.rates ) {
-                fx.rates = data.rates;
-                fx.base = data.base;
-            } else {
-                // If not, apply to fxSetup global:
-                var fxSetup = {
-                    rates : data.rates,
-                    base : data.base
-                }
-            }
-        });
+    var currency_data = data
+      if ( typeof fx !== "undefined" && fx.rates ) {
+        fx.rates = data.rates;
+        fx.base = data.base;
+      } else {
+        // If not, apply to fxSetup global:
+        var fxSetup = {
+          rates : data.rates,
+          base : data.base
+        }
+      }
+  });
 });
 
 function currency_exchange() {
-   $('#expense_usd_cost').val("")
-    var cost = $('#expense_cost').val();
-    var currency = $('#expense_currency_id option:selected').text()
-    var converted = fx.convert(cost, {from: currency, to: "USD"}).toFixed(2)
-    $('#expense_usd_cost').val(converted + " USD")
+ $('#expense_usd_cost').val("")
+ var cost = $('#expense_cost').val();
+ var currency = $('#expense_currency_id option:selected').text()
+ var converted = fx.convert(cost, {from: currency, to: "USD"}).toFixed(2)
+ $('#expense_usd_cost').val(converted + " USD")
 }
